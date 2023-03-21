5253db0ba0b8206f23c5a29b70d03922f7425a8e
scotthansforddev <128099046+scotthansforddev@users.noreply.github.com>
2023-03-21 07:09:59 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Main from "./components/Main";
+
+function App() {
+  return (
+  )
+    <div className="App">
+      <Header />
+      <div className="inputContainers">
+        <Main />
+      </div>
+    </div>
+  );
+}
+
+export default App;
